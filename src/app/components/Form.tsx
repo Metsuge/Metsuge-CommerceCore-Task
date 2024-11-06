281d91ca7fa7f3557394c41149e8d1e1037868ed
Metsuge <metsuge@gmail.com>
2024-11-06 17:27:45 +0200

    styling, dropdown, localStorage

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -49,26 +49,28 @@ export default function Form({ onFormDivPositionChange }: Props) {
     nameOnCard: ''
   });
   const [submitted, setSubmitted] = useState<boolean>(false);
-
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
   const divRef = useRef<HTMLInputElement>(null);
-
+  const [isChecked, setIsChecked] = useState(false);
 
 
   useEffect(() => {
+    const storedData = localStorage.getItem('formData');
+    if (storedData) {
+      setFormData(JSON.parse(storedData));
+    }
 
     const getPositions = () => {
       if (divRef.current) {
         const rect = divRef.current.getBoundingClientRect();
         onFormDivPositionChange(rect.left);
       }
-
-    }
+    };
     getPositions();
-
 
     window.addEventListener('resize', getPositions);
   }, []);
+  ;
 
 
 
@@ -84,9 +86,10 @@ export default function Form({ onFormDivPositionChange }: Props) {
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
     setSubmitted(true);
+
     // Validate form data using Zod schema
     const result = zodSchema.safeParse(formData);
-    console.log('result', result);
+    // console.log('result', result);
 
     if (!result.success) {
       // Extract and set error messages if validation fails
@@ -96,16 +99,21 @@ export default function Form({ onFormDivPositionChange }: Props) {
           validationErrors[err.path[0] as string] = err.message;
         }
       });
-      console.log(validationErrors);
+      // console.log(validationErrors);
 
       setErrors(validationErrors);
+      localStorage.setItem('formData', JSON.stringify(formData));
+      console.log("Saved form data to localStorage:", JSON.stringify(formData));
     } else {
       // Clear errors and proceed if validation passes
       setErrors({});
-      console.log("Form data is valid, proceeding with submission...");
-      // You can add further submission logic here
+      // console.log("Form data is valid, proceeding with submission...");
+
+      localStorage.setItem('formData', JSON.stringify(formData));
+      console.log("Saved form data to localStorage:", JSON.stringify(formData));
     }
   };
+
 
   const formatCardNumber = (value: string) => {
     // Remove any non-numeric characters
@@ -116,222 +124,247 @@ export default function Form({ onFormDivPositionChange }: Props) {
 
     return formattedValue;
   };
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(e.target.checked);
+  };
 
   return (
     <div ref={divRef} id={styles.form}>
 
       <form className={styles.formGroup} onSubmit={handleSubmit}>
-        <div className={styles.title}>Contact</div>
-        <div className={`${styles.formGroup} ${styles.email}`}>
-          <input
-            className={styles.input}
-            placeholder='Email Address'
-
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-
-          />
-          {errors.email && <span className={styles.error}>{errors.email}</span>}
-        </div>
-        <div className={styles.title}>Delivery</div>
-        {/* FIRST LAST NAMES */}
-        <div className={styles.formRow}>
-          <div>
+        <div id={styles.contact}>
+          <div className={styles.title}>Contact</div>
+          <div className={`${styles.formGroup} ${styles.email}`}>
             <input
               className={styles.input}
-              placeholder='First Name'
-              type="text"
-              id="firstName"
-              name="firstName"
-              value={formData.firstName}
+              placeholder='Email Address'
+
+              id="email"
+              name="email"
+              value={formData.email}
               onChange={handleChange}
 
             />
-            {errors.firstName && <span className={styles.error}>{errors.firstName}</span>}
-          </div>
-          <div>
-            <input
-              className={styles.input}
-              placeholder='Last Name'
-              type="text"
-              id="lastName"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleChange}
-
-            />
-            {errors.lastName && <span className={styles.error}>{errors.lastName}</span>}
+            {errors.email && <span className={styles.error}>{errors.email}</span>}
           </div>
         </div>
-
-        {/* Address */}
-        <div className={`${styles.formGroup} ${styles.address}`}>
-          <input
-            className={styles.input}
-            placeholder='Address'
-            type="text"
-            id="address"
-            name="address"
-            value={formData.address}
-            onChange={handleChange}
-
-          />
-          {errors.address && <span className={styles.error}>{errors.address}</span>}
-        </div>
-
-        {/* City */}
-        <div id={styles.cityStateRow} className={styles.formRow}>
-          <div id={styles.cityField}>
-            <input
-              className={styles.input}
-              placeholder='City'
-              type="text"
-              id="city"
-              name="city"
-              value={formData.city}
-              onChange={handleChange}
-
-            />
-            {errors.city && <span className={styles.error}>{errors.city}</span>}
-          </div>
-          {/* STATE */}
-          <div >
-            <select
-              className={`${styles.dropdown} ${styles.input}`}
-              id="state"
-              name="state"
-              value={formData.state}
-              onChange={handleChange}
-
-            >
-              <option value="1">State 1</option>
-              <option value="2">State 2</option>
-
-            </select>
-            {errors.state && <span className={styles.error}>{errors.state}</span>}
-          </div>
-          <div>
-            <input
-              className={styles.input}
-              placeholder='ZIP/Postal Code'
-              type="text"
-              id="zip"
-              name="zip"
-              value={formData.zip}
-              onChange={handleChange}
-
-            />
-            {errors.zip && <span className={styles.error}>{errors.zip}</span>}
-          </div>
-        </div>
-
-        <div className={`${styles.formGroup}  ${styles.country} `}>
-          <select
-            className={`${styles.dropdown} ${styles.input}`}
-            id="country"
-
-            name="country"
-            value={formData.country}
-            onChange={handleChange}
-
-          >
-            <option value="United States">United States</option>
-            <option value="Canada">Canada</option>
-            <option value="United Kingdom">United Kingdom</option>
-            <option value="Australia">Australia</option>
-            <option value="Other">Other</option>
-          </select>
-          {errors.country && <span className={styles.error}>{errors.country}</span>}
-        </div>
-
-        <div className={styles.title}>Payment</div>
-        <div className={styles.h2}>All transactions are secure and encrypted.</div>
-        <div className={styles.formGroup} >
-          <label className="circular-checkbox">
-
-            <div className={styles.cardIcons}>
-              <input type="checkbox" id="circularCheckbox" />
-              <span className="checkmark"> Credit Card</span>
-              <div className={styles.icons}>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-              </div>
-            </div>
-
-          </label>
-          {/* CARD NR */}
-          <div className={`${styles.formGroup}`}>
-            <input
-              className={styles.input}
-              placeholder='Card number'
-              type="text"
-              id="cardNr"
-              name="cardNr"
-              value={formData.cardNr}
-              onChange={handleChange}
-
-            />
-            {errors.cardNr && <span className={styles.error}>{errors.cardNr}</span>}
-          </div>
-          {/* EXPIRATION SECURITY */}
-
+        <div id={styles.delivery}>
+          <div className={styles.title}>Delivery</div>
+          {/* Names */}
           <div className={styles.formRow}>
             <div>
               <input
                 className={styles.input}
-                placeholder='Expiration (MM/YY)'
+                placeholder='First Name'
                 type="text"
-                id="expiration"
-                name="expiration"
-                value={formData.expiration}
+                id="firstName"
+                name="firstName"
+                value={formData.firstName}
                 onChange={handleChange}
 
               />
-              {errors.expiration && <span className={styles.error}>{errors.expiration}</span>}
+              {errors.firstName && <span className={styles.error}>{errors.firstName}</span>}
             </div>
             <div>
               <input
                 className={styles.input}
-                placeholder='Security code'
+                placeholder='Last Name'
                 type="text"
-                id="securityCode"
-                name="securityCode"
-                value={formData.securityCode}
+                id="lastName"
+                name="lastName"
+                value={formData.lastName}
                 onChange={handleChange}
 
               />
-              {errors.securityCode && <span className={styles.error}>{errors.securityCode}</span>}
+              {errors.lastName && <span className={styles.error}>{errors.lastName}</span>}
+            </div>
+          </div>
+
+          {/* Address */}
+          <div className={`${styles.formGroup} ${styles.address}`}>
+            <input
+              className={styles.input}
+              placeholder='Address'
+              type="text"
+              id="address"
+              name="address"
+              value={formData.address}
+              onChange={handleChange}
+
+            />
+            {errors.address && <span className={styles.error}>{errors.address}</span>}
+          </div>
+
+          {/* City */}
+          <div id={styles.cityStateRow} className={styles.formRow}>
+            <div id={styles.cityField}>
+              <input
+                className={styles.input}
+                placeholder='City'
+                type="text"
+                id="city"
+                name="city"
+                value={formData.city}
+                onChange={handleChange}
+
+              />
+              {errors.city && <span className={styles.error}>{errors.city}</span>}
+            </div>
+            {/* STATE */}
+            <div >
+              <select
+                className={`${styles.dropdown} ${styles.input}`}
+                id="state"
+                name="state"
+                value={formData.state}
+                onChange={handleChange}
+
+              >
+                <option value="1">State 1</option>
+                <option value="2">State 2</option>
+
+              </select>
+              {errors.state && <span className={styles.error}>{errors.state}</span>}
+            </div>
+            <div>
+              <input
+                className={styles.input}
+                placeholder='ZIP/Postal Code'
+                type="text"
+                id="zip"
+                name="zip"
+                value={formData.zip}
+                onChange={handleChange}
+
+              />
+              {errors.zip && <span className={styles.error}>{errors.zip}</span>}
+            </div>
+          </div>
+
+          <div className={`${styles.formGroup}  ${styles.country} `}>
+            <select
+              className={`${styles.dropdown} ${styles.input}`}
+              id="country"
+
+              name="country"
+              value={formData.country}
+              onChange={handleChange}
+
+            >
+              <option value="United States">United States</option>
+              <option value="Canada">Canada</option>
+              <option value="United Kingdom">United Kingdom</option>
+              <option value="Australia">Australia</option>
+              <option value="Other">Other</option>
+            </select>
+            {errors.country && <span className={styles.error}>{errors.country}</span>}
+          </div>
+        </div>
+        <div id={styles.payment}>
+          <div className={styles.title}>Payment</div>
+          <div className={styles.h2}>All transactions are secure and encrypted.</div>
+          <div id={styles.cardDiv} className={styles.formGroup} >
+            <label className="circular-checkbox">
+
+              <div className={styles.cardIcons}>
+                <input
+                  type="checkbox"
+                  id="circularCheckbox"
+                  className={styles.checkbox}
+                  onChange={handleCheckboxChange} />
+
+                <span
+
+                  style={{
+                    marginTop: '4px'
+                  }}
+
+                  className={`${styles.checkmark} ${isChecked ? styles.checked : ''}`
+                  }
+
+                > Credit Card</span>
+                <div className={`${styles.icons} ${isChecked ? styles.iconsChecked : ''}`}>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+              </div>
+
+            </label>
+            {/* CARD NR */}
+            <div className={`${styles.formGroup}`}>
+              <input
+                className={styles.input}
+                placeholder='Card number'
+                type="text"
+                id="cardNr"
+                name="cardNr"
+                value={formData.cardNr}
+                onChange={handleChange}
+
+              />
+              {errors.cardNr && <span className={styles.error}>{errors.cardNr}</span>}
+            </div>
+            {/* EXPIRATION SECURITY */}
+
+            <div className={styles.formRow}>
+              <div>
+                <input
+                  className={styles.input}
+                  placeholder='Expiration (MM/YY)'
+                  type="text"
+                  id="expiration"
+                  name="expiration"
+                  value={formData.expiration}
+                  onChange={handleChange}
+
+                />
+                {errors.expiration && <span className={styles.error}>{errors.expiration}</span>}
+              </div>
+              <div>
+                <input
+                  className={styles.input}
+                  placeholder='Security code'
+                  type="text"
+                  id="securityCode"
+                  name="securityCode"
+                  value={formData.securityCode}
+                  onChange={handleChange}
+
+                />
+                {errors.securityCode && <span className={styles.error}>{errors.securityCode}</span>}
+
+              </div>
+            </div>
+
+            {/* NAME ON CARD */}
+            <div className={`${styles.formGroup}`}>
+              <input
+                className={styles.input}
+                placeholder='Name on card'
+                type="text"
+                id="nameOnCard"
+                name="nameOnCard"
+                value={formData.nameOnCard}
+                onChange={handleChange}
+
+              />
+              {errors.nameOnCard && <span className={styles.error}>{errors.nameOnCard}</span>}
 
             </div>
           </div>
 
-          {/* NAME ON CARD */}
-          <div className={`${styles.formGroup}`}>
-            <input
-              className={styles.input}
-              placeholder='Name on card'
-              type="text"
-              id="nameOnCard"
-              name="nameOnCard"
-              value={formData.nameOnCard}
-              onChange={handleChange}
-
-            />
-            {errors.nameOnCard && <span className={styles.error}>{errors.nameOnCard}</span>}
-
+          <button
+            className={`${styles.button}`}
+          >COMPLETE ORDER</button>
+          <div className={styles.endText}>
+            <div className={styles.endTextInner}><span><svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M11.9583 5.25H11.0833V4.08332C11.0833 1.83176 9.25159 0 7 0C4.74841 0 2.91668 1.83176 2.91668 4.08332V5.25H2.04168C1.88046 5.25 1.75 5.38046 1.75 5.54168V12.8334C1.75 13.4768 2.27322 14 2.91668 14H11.0834C11.7268 14 12.25 13.4768 12.25 12.8333V5.54168C12.25 5.38046 12.1195 5.25 11.9583 5.25ZM7.8733 11.3428C7.88241 11.4251 7.85594 11.5077 7.80068 11.5696C7.74542 11.6313 7.66623 11.6667 7.58335 11.6667H6.41668C6.3338 11.6667 6.25461 11.6313 6.19935 11.5696C6.14409 11.5078 6.11759 11.4251 6.12672 11.3428L6.31072 9.68825C6.01193 9.47092 5.83335 9.12712 5.83335 8.75C5.83335 8.10657 6.35657 7.58332 7.00003 7.58332C7.64348 7.58332 8.1667 8.10655 8.1667 8.75C8.1667 9.12712 7.98812 9.47092 7.68934 9.68825L7.8733 11.3428ZM9.33332 5.25H4.66668V4.08332C4.66668 2.79675 5.71342 1.75 7 1.75C8.28658 1.75 9.33332 2.79675 9.33332 4.08332V5.25Z" fill="#828282" />
+            </svg>
+            </span >
+              All transactions are secure and encrypted</div>
           </div>
-        </div>
-
-        <button
-          className={`${styles.button}`}
-        >COMPLETE ORDER</button>
-        <div className={styles.endText}>
-          <div className={styles.endTextInner}><span></span>
-            All transactions are secure and encrypted</div>
         </div>
       </form>
 
--- src/app/components/ProductOverview.tsx
+++ src/app/components/ProductOverview.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from "./ProductOverview.module.scss"
 
 
@@ -9,7 +9,13 @@ interface Props {
 
 const ProductOverview = ({ onOverviewDivPositionChange }: Props) => {
   const divRef = useRef<HTMLInputElement>(null);
+  const [isExpanded, setIsExpanded] = useState(false);
 
+
+
+  const dropdown = () => {
+    setIsExpanded(!isExpanded);
+  };
 
   useEffect(() => {
 
@@ -28,37 +34,66 @@ const ProductOverview = ({ onOverviewDivPositionChange }: Props) => {
 
 
   return (
-    <div ref={divRef} className={styles.ProductOverview}>
+    <div >
+      <div ref={divRef} id={styles.ProductOverview}
+        style={{
+          marginBottom: isExpanded ? '16px' : 0
+        }}
+      >
 
-      <table>
-        <tbody>
-          <tr>
-            <td id={styles.tableItem}>
-              <div className={styles.image}>
-                <div className={styles.itemCount}>
-                  <div className={styles.itemCountInner}>3</div>
+        <table>
+          <tbody>
+
+            <tr id={styles.dropdown} >
+              <td style={{
+                borderBottom: isExpanded ? '1px solid #E0E0E0' : '0'
+              }}>
+                Order overview
+                <span id={styles.dropdownArrow} onClick={dropdown} className={isExpanded ? styles.arrowDown : styles.arrowRight}><svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M6.00002 3C6.1882 3 6.37636 3.07203 6.51983 3.21578L11.0346 7.74168C11.3218 8.02959 11.3218 8.49637 11.0346 8.78416C10.7475 9.07195 10.282 9.07195 9.99475 8.78416L6.00002 4.77939L2.00526 8.78402C1.71807 9.07181 1.25256 9.07181 0.965503 8.78402C0.678165 8.49623 0.678166 8.02945 0.965503 7.74154L5.48021 3.21564C5.62375 3.07186 5.81191 3 6.00002 3Z" fill="#828282" />
+                </svg>
+                </span>
+              </td>
+
+              <td
+                style={{
+                  borderBottom: isExpanded ? '1px solid #E0E0E0' : '0'
+                }}
+              >$299.97</td>
+            </tr>
+
+            {/* <div className={isExpanded ? styles.expand : styles.collapse}> */}
+            <tr className={isExpanded ? styles.expand : styles.collapse}>
+              <td id={styles.tableItem}>
+                <div className={styles.image}>
+                  <div className={styles.itemCount}>
+                    <div className={styles.itemCountInner}>3</div>
+                  </div>
                 </div>
-              </div>
-              <div className={styles.title}>LogoIpsum IPL</div>
-            </td>
-            <td>
-              $299.97
-            </td>
-          </tr>
+                <div className={styles.title}>LogoIpsum IPL</div>
+              </td>
+
+              <td id={styles.tableItemPrice}>
+                $299.97
+              </td>
+            </tr>
 
 
-          <tr>
-            <td>Subtotal</td>
-            <td>$299.97</td>
-          </tr>
+            <tr className={isExpanded ? styles.expand : styles.collapse}>
+              <td>Subtotal</td>
+              <td>$299.97</td>
+            </tr>
 
 
-          <tr>
-            <td>Total</td>
-            <td>$299.97</td>
-          </tr>
-        </tbody>
-      </table>
+            <tr className={isExpanded ? styles.expand : styles.collapse}>
+              <td>Total</td>
+              <td>$299.97</td>
+            </tr>
+            {/* </div> */}
+
+          </tbody>
+        </table>
+      </div>
     </div >
   )
 }
